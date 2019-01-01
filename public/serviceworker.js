41d758a57a85b261ac5b5e29a9dfe5a433b1c075
Sung Kim <sungmkim80@gmail.com>
2019-01-01 18:40:44 -0500

    Added response when the site is offline

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -5,6 +5,19 @@ addEventListener("activate", function(event) {
   console.log(`The service worker is ACTIVATing`);
 });
 addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(new Response("hello, world!"));
+  const { request } = fetchEvent;
+  fetchEvent.respondWith(
+    fetch(request)
+      .then(response => response)
+      .catch(
+        error =>
+          new Response(
+            `<h1>Oop!</h1> <div>something went wrong!</div> <div>${error}</div>`,
+            {
+              headers: { "Content-type": "text/html;charset=utf-8" }
+            }
+          )
+      )
+  );
   //   console.log(fetchEvent.request);
 });
